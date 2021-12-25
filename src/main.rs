c4f18846dcffa7725a0cf9109999a0071decb20f
Kosuke Yamashita <ochaochaocha3@gmail.com>
2021-12-25 20:12:44 +0900

    Fix BSP name: pico to rp-pico, rp_pico
    BSP rp-pico has been renamed from pico.

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ use panic_probe as _;
 
 // Provide an alias for our BSP so we can switch targets quickly.
 // Uncomment the BSP you included in Cargo.toml, the rest of the code does not need to change.
-use pico as bsp;
+use rp_pico as bsp;
 // use pro_micro_rp2040 as bsp;
 
 use bsp::hal::{
